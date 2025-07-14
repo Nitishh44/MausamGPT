6563bc1d5dbcdabddaaed8ed6efcb391a8ffce6c
Nitish Gupta <guptanitish526@gmail.com>
2025-07-14 16:00:12 +0530

    Update user Detect Location

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getWeather() {
   const city = document.getElementById("cityInput").value;
-  const apiKey = "40b30c129c3c8704a652160300e6957a";
+  const apiKey = API_KEY;
   if (!city) {
     alert("plse enter a city name");
     return;
@@ -52,4 +52,81 @@ async function getWeather() {
     const min = time.getMinutes().toString().padStart(2, "0");
     sunrise.textContent = `${hrs}:${min} AM`;
   }
+}
+async function fetchCitySuggestions(query) {
+  const apiUrl = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${query}&limit=5&sort=-population`;
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key":
+        GEO_API_KEY,
+        "X-RapidAPI-Host":
+        "wft-geo-db.p.rapidapi.com"
+      }
+    });
+    const data = await response.json();
+    return data.data;
+
+  } catch (error) {
+    console.error("City suggestions error:", error);
+    return [];
+  }
+}
+const cityInput = document.getElementById("cityInput");
+const citySuggestions = document.getElementById("citySuggestions");
+
+cityInput.addEventListener("input", async () => {
+  const query = cityInput.value.trim();
+
+  if (query.length < 2) return;
+
+  const cities = await fetchCitySuggestions(query);
+  citySuggestions.innerHTML = "";
+
+  cities.forEach(city => {
+    const option = document.createElement("option");
+    option.value = `${city.city}, ${city.countryCode}`;
+    citySuggestions.appendChild(option);
+  });
+});
+async function handleCityInput() {
+  const query = cityInput.value.trim();
+
+  if (query.length < 2) return;
+  const cities = await fetchCitySuggestions(query);
+  citySuggestions.innerHTML = "";
+  cities.forEach(city => {
+    const option = document.createElement("Option");
+    option.value = `${city.city}, ${city.countryCode}`;
+    citySuggestions.appendChild(option);
+  })
+}
+document.getElementById("detectLocationBtn").addEventListener("click", getUserLocation);
+function getUserLocation(){
+  if("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        getWeatherByCoords(lat, lon);
+      }, error => {
+        console.error("Location error:", error);
+        alert("Unable to fetch your location");
+      }
+    );
+  } else {
+    alert("Geolocation not supported in your browser.");
+  }
+}
+async function getWeatherByCoords(lat, lon) {
+ const apikey = API_KEY;
+ const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`;
+ try {
+  const response = await fetch(url);
+  const data = await response.json();
+  showWeather(data);
+ } catch (error) {
+  console.error("Error fetching weather by location:", error);
+ }
 }
